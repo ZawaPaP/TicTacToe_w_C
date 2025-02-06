18bf9a364b7e95f9cef7165632abdc9d9dbf9a78
ZawaPaP <cqcsolid@gmail.com>
2025-02-07 00:07:02 +0900

    Added feature to judge if the game finished or not.

--- tests/test_game.c
+++ tests/test_game.c
@@ -3,28 +3,93 @@
 #include <string.h>
 #include <assert.h>
 #include "../include/board.h"
-#include "../include/plays.h"
 #include "../include/game.h"
+#include "test_utils.h"
 #include "test_game.h"
 
 
 
-/* void testHasWinnerExpected()
+void testHasWinnerInVerticalExpected()
  {
+    Board board_1 = __prepareBoard();
+
+    board_1.cells[1][1] = PLAYER_X;
+    board_1.cells[2][1] = PLAYER_X;
+    board_1.cells[3][1] = PLAYER_X;
+    board_1.cells[4][1] = PLAYER_X;
+    board_1.cells[5][1] = PLAYER_X;
+
+    assert(hasWinner(&board_1) == 1);
+
+    Board board_2 = __prepareBoard();
+
+    board_2.cells[4][7] = PLAYER_O;
+    board_2.cells[5][7] = PLAYER_O;
+    board_2.cells[6][7] = PLAYER_O;
+    board_2.cells[7][7] = PLAYER_O;
+    board_2.cells[8][7] = PLAYER_O;
+
+    assert(hasWinner(&board_2) == 1);
+
+ }
+
+void testHasWinnerInHorizontalExpected() {
+    Board board_1 = __prepareBoard();
+
+    board_1.cells[1][1] = PLAYER_X;
+    board_1.cells[1][2] = PLAYER_X;
+    board_1.cells[1][3] = PLAYER_X;
+    board_1.cells[1][4] = PLAYER_X;
+    board_1.cells[1][5] = PLAYER_X;
+
+    assert(hasWinner(&board_1) == 1);
+
+    Board board_2 = __prepareBoard();
+
+    board_2.cells[8][5] = PLAYER_O;
+    board_2.cells[8][6] = PLAYER_O;
+    board_2.cells[8][7] = PLAYER_O;
+    board_2.cells[8][8] = PLAYER_O;
+    board_2.cells[8][9] = PLAYER_O;
+
+    assert(hasWinner(&board_2) == 1);
+}
+
+void testHasWinnerInCrossExpected() {
+    Board board = __prepareBoard();
+    board.cells[1][1] = PLAYER_X;
+    board.cells[2][2] = PLAYER_X;
+    board.cells[3][3] = PLAYER_X;
+    board.cells[4][4] = PLAYER_X;
+    board.cells[5][5] = PLAYER_X;
+
+    assert(hasWinner(&board) == 1);
+}
+
+void testNotHasWinnerExpected() {
     Board board = __prepareBoard();
 
     board.cells[1][1] = PLAYER_X;
-    board.cells[2][1] = PLAYER_X;
-    board.cells[3][1] = PLAYER_X;
-    board.cells[4][1] = PLAYER_X;
-    board.cells[5][1] = PLAYER_X;
+    board.cells[2][2] = PLAYER_O;
+    board.cells[3][3] = PLAYER_X;
+    board.cells[4][4] = PLAYER_X;
+    board.cells[5][4] = PLAYER_X;
+    board.cells[5][5] = PLAYER_X;
+    board.cells[5][6] = PLAYER_X;
+    board.cells[6][6] = PLAYER_X;
+    board.cells[7][7] = EMPTY_CELL;
+    board.cells[8][8] = PLAYER_X;
+    
 
-    assert(hasWinner(&board) == 1);
- } */
+    assert(hasWinner(&board) == 0);
+}
 
 void runGameTests() {
     printf("Start runGameTests...\n");
-    //testHasWinnerExpected();
+    testHasWinnerInVerticalExpected();
+    testHasWinnerInHorizontalExpected();
+    testHasWinnerInCrossExpected();
+    testNotHasWinnerExpected();
     printf("Finished runGameTests.\n");
 }
 
---
+++ tests/test_utils.h
@@ -0,0 +1,7 @@
+#ifndef TEST_UTILS_H
+#define TEST_UTILS_H
+
+#include "../include/board.h"
+Board __prepareBoard();
+
+#endif
