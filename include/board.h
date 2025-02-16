bc1dbfa7a1ac649b7a78690788b388f44480c7c7
ZawaPaP <cqcsolid@gmail.com>
2025-02-16 14:19:39 +0900

    continue move directory

--- include/board.h
+++ include/board.h
@@ -8,4 +8,7 @@ BOOL isInBoard(int r, int c);
 BOOL isWinMove(Board *board, int r, int c, char playerMark);
 BOOL __isSameLine(Board *board, lineInfo *line1, lineInfo *line2, int row, int col, int dx, int dy);
 lineInfoArray __getTargetLengthLinesInDirectionCandidates(Board *board, int row, int col, int length, int dx, int dy, char playerMark);
+BOOL __wouldCreateOverline(Board *board, int row, int col, int dx, int dy, char playerMark);
+lineInfoArray __getTargetLengthLinesInDirection(Board *board, int row, int col, int length, int dx, int dy, char playerMark);
+BOOL __isDoubleThree(Board *board, int x, int y);
 #endif
--- include/game.h
+++ include/game.h
@@ -4,9 +4,5 @@
 
 void
 runGameLoop();
-char getWinner(Board *board);
 BOOL isGameOver(Board *board, int *row, int *col, char player);
-BOOL __wouldCreateOverline(Board *board, int row, int col, int dx, int dy, char playerMark);
-lineInfoArray __getTargetLengthLinesInDirection(Board *board, int row, int col, int length, int dx, int dy, char playerMark);
-BOOL __isDoubleThree(Board *board, int x, int y);
 #endif
